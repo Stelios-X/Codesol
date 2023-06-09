43ad2f891d98cf95bf3c721b3b2ab9a9d114b587
Bobby Brandon Angote <ebobby@outlook.com>
2023-06-09 10:16:54 +0300

    Update on the Two-sum close to zero solution

--- ConsoleApp1/Delta.cs
+++ ConsoleApp1/Delta.cs
@@ -12,37 +12,37 @@ namespace ConsoleApp1
         {
             int sum = 0;
             int counter = 0;
-            int[] my_array = { 4, 2, 3, 7, 9, 1};
+            int[] my_array = { 4, 2, 3, 7, 9, 1 };
             foreach (int i in my_array)
             {
                 //Console.WriteLine(i + "The winner is also " + my_array[counter]);
-                Console.WriteLine(sum = i + my_array[counter+1]);
+                Console.WriteLine(sum = i + my_array[counter + 1]);
                 counter++;
-                
+
             }
         }
 
-        public void Minor(int[] the_array, int the_length) 
+        public void Minor(int[] the_array, int the_length)
         {
             int first_index = 0;
             int second_index = 0;
-            int base_sum = the_array[0]+ the_array[1];
+            int base_sum = the_array[0] + the_array[1];
 
 
 
             if (the_length < 2)
             {
-                Console.WriteLine("Unfortunately, this won't work B");
+                Console.WriteLine("Invalid array provided");
             }
-            else 
+            else
             {
                 for (int i = 0; i < the_length; i++)
                 {
-                    for (int j = i+1; 0 < the_length; j++)
+                    for (int j = i + 1; 0 < the_length; j++)
                     {
                         int new_sum = the_array[i] + the_array[j];
 
-                        if (new_sum < base_sum) 
+                        if (new_sum < base_sum)
                         {
                             base_sum = new_sum;
                             first_index = i;
@@ -51,13 +51,14 @@ namespace ConsoleApp1
                     }
                 }
 
-                Console.WriteLine("The 2 indices are " + first_index + " and "+second_index+ " the summation of their values gives "+base_sum);
-            }    
+                Console.WriteLine("The 2 indices are " + first_index + " and " + second_index + " the summation of their values gives " + base_sum);
+            }
         }
 
-        public void find_num_pair(int[] array, int length) 
+        // This method returns the sum of 2 pairs of elements in an array that's closest to zero
+        public void find_num_pair(int[] array, int length)
         {
-            if (length <2 ) 
+            if (length < 2)
             {
                 Console.WriteLine("Invalid input");
                 return;
@@ -69,11 +70,11 @@ namespace ConsoleApp1
 
             for (int i = 0; i < length; i++)
             {
-                for (int j = i+1; j < length; j++)
+                for (int j = i + 1; j < length; j++)
                 {
                     new_sum = array[i] + array[j];
 
-                    if (Math.Abs(new_sum) < Math.Abs(min_sum)) 
+                    if (Math.Abs(new_sum) < Math.Abs(min_sum))
                     {
                         min_sum = new_sum;
 
@@ -83,11 +84,124 @@ namespace ConsoleApp1
                 }
             }
 
-            Console.Write("The two indices who's sum returns a value closest to zero are "+min_index1+" and "+min_index2+ "\n;the sum is "+min_sum+". The numbers are " + array[min_index1]+" and " + array[min_index2]);
+            Console.Write("The two indices who's sum returns a value closest to zero are " + min_index1 + " and " + min_index2 + "\n;the sum is " + min_sum + ". The numbers are " + array[min_index1] + " and " + array[min_index2]);
 
         }
 
-        
+        //This method finds the sum of all repeated elements in two arrays
+        public void find_non_overlap(int[] arr1, int[] arr2)
+        {
+            if (arr1 != null || arr2 != null)
+            {
+                int sum = 0;
+                int sum2 = 0;
+
+                foreach (int i in arr1)
+                {
+                    for (int j = 0; j < arr1.Length; j++)
+                    {
+                        if (i == arr2[j])
+                        {
+                            sum2 = i;
+                            sum = sum + arr2[j];
+                        }
+                    }
+                }
+                Console.Write(sum + sum2);
+
+
+
+            }
+        }
+
+
+
+        //This method buys and sells stock to maximise profit
+        public void StockBroker(int[] my_array)
+        {
+            int maxprofit = 0;
+            int minPrice = int.MaxValue;
+
+            for (int i = 0; i < my_array.Length; i++)
+            {
+                if (my_array[i] < minPrice) 
+                {
+                    minPrice = my_array[i]; 
+                }
+                int profit = my_array[i] - minPrice;
+
+                if (profit > maxprofit)
+                {
+                    maxprofit = profit;
+                }
+            }
+            Console.Write(maxprofit);
+        }
+
+        //Method that buys stock at low and sells at high given an array of prices and returns the max profit 
+        public void StockBroker_v2(int[] my_array)
+        {
+            int maxprofit = 0;
+            int minPrice = int.MaxValue;
+            int counter = 0;
+
+            foreach (var i in my_array)
+            {
+                if (i < minPrice)
+                {
+                    minPrice = i;
+                }
+                int profit = i - minPrice;
+
+                if (profit > maxprofit)
+                {
+                    maxprofit = profit;
+                }
+                counter++;
+            }
+            
+            Console.Write(maxprofit);
+        }
+
+        //This method buys and sells stocks every single day when profitable and updates the maximum profit cumulatively
+
+        //Function to return the maximum profit that can be made after buying and selling the given stocks
+        static void maxProfit(int[] price,int start, int end) 
+        {
+            //If the stocks can't be bought
+            if (end <= start) 
+            {
+                //return 0;
+                Console.Write("Stock can't be purchased");
+            }
+
+            //Initialize the profit
+            int profit = 0;
+
+
+            //The day at which the stock 
+            for (int i = start; i < end; i++)
+            {
+                for (int j = i+1; j <= end; j++)
+                {
+                    //If buying the stock on the ith day and selling it on the jth day is profitable
+                    if (price[j] > price[i]) 
+                    {
+                        //Update the current profit
+                        int curr_profit = price[j] - price[i] + maxProfit(price, start, i - 1) + maxProfit(price, j + 1, end);
+
+                        // Update the maximum profit so far
+                        profit = Math.Max(profit, curr_profit);
+
+                    }
+
+                }
+            }
+            Console.Write(maxProfit(price, 0, n - 1));
+
+        }
+
+
 
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -24,13 +24,20 @@ int[] arr1 = {25,42,-31,12,55,-20,-18,-48,16,34 };
 
 int[] arr3 = {1, 2, 3, 14, 7, 10};
 
+int[] arr_a = { 1, 5, 3, 8 };
+int[] arr_b = { 5, 4, 3, 7 };
+
+int[] sb_arr = {7,1,5,3,6,4};
+//int[] arr_a = { 6, 2, 3, 4 };
+//int[] arr_b = { 4, 5, 4, 4 };
+
  
 
-/*
+
 Delta mydel = new Delta();
 
-mydel(arr3);*/
+mydel.StockBroke(sb_arr);
 
-
+/*
 Summation summation = new Summation();
-summation.Two_sum_zero_solution(arr, 6);
+summation.Two_sum_zero_solution(arr, 6);*/
